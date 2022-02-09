054384fa2db61a261a24771ec7b48b58182fb6dc
aleja5313 <94092844+aleja5313@users.noreply.github.com>
2022-02-08 22:21:58 -0500

    Add files via upload

--- script/detailPerros.js
+++ script/detailPerros.js
@@ -1,20 +1,22 @@
+const listador2 = document.querySelector('body')
 
-const listador = document.querySelector('body')
 
-
-const getLocalStorage = () => {
+const getLocalStorage2 = () => {
     
+    const arrayLike = [];
     const det = JSON.parse(localStorage.getItem("Dogs")); 
-    const { imagen, nombre, raza, edad,ubicacion, personalidad,historia,imgusuario,usuario,} = det
+    const {id, imagen, nombre, raza, edad,ubicacion, personalidad,historia,imgusuario,usuario, categoria } = det
 
-    listador.innerHTML += `
+    listador2.innerHTML += `
  
-    <div class="card-body" >
-        
-  
-        <a href="#"><img src=${imagen} class="card-img-top" alt="..."></a>
-        
-        <img class="favorit" src="./image/Union1.png" alt="">
+
+    <div class="card-body" >  
+
+     
+    <a href="../index3.html"><button class="btnBack"><img src="../image/atras.png" alt="logo atrás"></button></a>
+         
+    <a href="#"><img src=${imagen} class="card-img-top" alt="..."></a>
+    <button style="float: right"  class="favDog"> <img src="../image/Union1.png" alt="" style="float: right" ></button>
     
         <div>
             <img src="./image/male (1).png" style="position: absolute;
@@ -53,28 +55,75 @@ const getLocalStorage = () => {
         <h2>Historia</h2>
         <p style="color: #535353;">Historia ${historia}</p>
         <div>
-    
+
         <a href="#"><img src=${imgusuario} class="card-logo" alt="..."  style="float: left;"></a>
         <strong> <p style="color: #535353;">Publicado Por</p></strong>
         <p style="color: #535353;">  ${usuario}</p>
     </div>
-    <button class="contact"><a class="a-inicio-sig" href="./chat.html" >Contactar</a></button>
+    <button class="contact"><a class="a-inicio-sig" href="./chat.html">Contactar</a></button>
+
+                    <!-- <h4 style="color: #535353;"> categoria:  ${categoria}</h4> -->
                 
                 </div>
                 
     </div>
 
    `
-}
 
-document.addEventListener('DOMContentLoaded', getLocalStorage)
+const btnLike = document.querySelector('.favDog');
+  
+    btnLike.addEventListener('click', () => {
+    let likeName = nombre;
+    let likeImg = imagen;
+    let likeRace = raza;
+    let likeId = id;
+    let likeCategory = categoria;
 
+    let like = { 
+      likeName,
+      likeImg,
+      likeRace,
+      likeId,
+      likeCategory
+    }
+
+    console.log(like)
+
+     const key = JSON.parse(localStorage.getItem('Fav'))
+
+     if (btnLike.classList.contains('favDog')) {
+    
+
+       btnLike.classList.remove("favDog");
+       btnLike.classList.add("circlePush");
+
+       if (key !== null) { 
+           key.push(like) 
+           localStorage.setItem('Fav', JSON.stringify(key)) 
+         } else { 
+           arrayLike.push(like); 
+           localStorage.setItem('Fav', JSON.stringify(arrayLike))
+         }
+
+     }  else if (btnLike.classList.contains('favDog')) {
+         btnLike.classList.remove("circlePush");
+         btnLike.classList.add("circleLike");
+      
+         const dog = key.filter(categ => categ.likeCategory === 'dog')
+
+        }
+      }
+      )
+    }
+
+
+document.addEventListener('DOMContentLoaded', getLocalStorage2)
 
 
 listador.addEventListener('click', (e) => {
 
     if (e.target.classList.contains('card-img-top')) {
-        window.location.href = "index.html";
+        window.location.href = "index3.html";
     }
 
 })
